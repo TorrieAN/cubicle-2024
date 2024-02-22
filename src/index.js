8835c6c6b5e43c9ebde4d3326fc83de1087cff2a
TorrieAN <victoria.n.atanasova@gmail.com>
2024-02-22 15:37:15 +0200

    Add 404

--- src/index.js
+++ src/index.js
@@ -13,6 +13,9 @@ hbsConfig(app);
 
 app.use(homeController);
 app.use('/cubes', cubeController);
+app.get('*', (req, res) => {
+    res.redirect('/404');
+});
 
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
