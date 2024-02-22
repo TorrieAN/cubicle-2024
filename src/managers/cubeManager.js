f5d6a0822d6f3a9ff5c278017e54d4d09cd89b23
TorrieAN <victoria.n.atanasova@gmail.com>
2024-02-22 15:22:18 +0200

    Add details page for cubes

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,11 +1,29 @@
 const uniqid = require('uniqid');
-const cubes = [];
+const cubes = [
+    {
+        id: '2wdw12g0ylsx8gzjg',
+        name: "Mirror Cube",
+        description: "The Mirror Cube is an unofficial puzzle. It has pieces of different shapes which allows it to shape shift. When you scramble it, the puzzle takes a chaotic form and loses its cubic shape which makes the solving process quite complicated.",
+        imageUrl: 'https://logicbg.com/wp-content/uploads/2020/03/rubik-kub-Mirror-Cube-Silver-QiYi-razbarkano-kubche.jpg',
+        difficultyLevel: 4
+    },
+
+    {
+        id: '2wdw12g0ylsx6hzjg',
+        name: "Phantom Cube",
+        description: "Touch, reveal, and solve with the Rubik’s Phantom. ou will be amazed by the thermochromic technology of this Cube. Reveal the Phantom’s colors with the heat of your touch.",
+        imageUrl: 'https://cdn.spinmasterstudios.com/products/rubiks/us/778988428757/full1.jpg',
+        difficultyLevel: 3
+    }
+];
 
 exports.getAll = () => cubes.slice();
 
+exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId); 
+
 exports.create = (cubeData) => {
     const newCube = {
-        id: uniqid,
+        id: uniqid(),
         ...cubeData,
     };
 
