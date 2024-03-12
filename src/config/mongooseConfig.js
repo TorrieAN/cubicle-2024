59f13cf54d26a3ce82bea17ca38d2cd97abaf910
TorrieAN <victoria.n.atanasova@gmail.com>
2024-03-12 14:21:26 +0200

    Mongoose Setup

---
+++ src/config/mongooseConfig.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const uri = 'mongodb://127.0.0.1:27017/cubicle-2024';
+
+async function dbConnect() {
+    await mongoose.connect(uri);
+    console.log('Database connected');
+}
+
+module.exports = dbConnect;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ const express = require('express');
 
 const expressConfig = require('./config/expressConfig');
 const hbsConfig = require('./config/hbsConfig');
+const dbConnect = require('./config/mongooseConfig');
 const routes = require('./routes');
 
 const app = express();
 const PORT = 5000;
 
-expressConfig(app);
-hbsConfig(app);
+dbConnect()
+    .then(() => {
+        console.log('DB connected sucessfully');
+        expressConfig(app);
+        hbsConfig(app);
 
-app.use(routes);
+        app.use(routes);
+        app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
+    })
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
+    .catch(err => {
+        console.log('DB error:', err);
+    });
