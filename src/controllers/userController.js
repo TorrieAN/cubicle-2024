883ac93be0a9eaf55c91d9dd7e48bb09fc87db4c
TorrieAN <victoria.n.atanasova@gmail.com>
2024-03-26 13:46:45 +0200

    Set user auth token be saved as a cookie

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const userManager = require('../managers/userManager');
 
+
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
@@ -20,9 +21,9 @@ router.get('/login', (req, res) => {
 router.post('/login', async (req, res) => {
     const {username, password} = req.body;
     
-    const user = await userManager.login(username, password);
+    const token = await userManager.login(username, password);
     
-    console.log(user);
+    res.cookie('userToken', token, { httpOnly: true });
     res.redirect('/');
 })
 
--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,5 +1,8 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('../lib/jwt');
+
+const SECRET = 'hereisthesecret';
 
 exports.register = (userData) => User.create(userData);
 
@@ -16,5 +19,12 @@ exports.login = async (username, password) => {
         throw new Error('Incorrect username or password');
     }
 
-    return user;
+    const payload = {
+        _id: user._id,
+        username: user.username,
+    }
+
+    const token = await jwt.sign(payload, SECRET, { expiresIn: '2d' });
+
+    return token;
 }
