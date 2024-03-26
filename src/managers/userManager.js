fa7ab5f8f7a84cebbda2892ba100f0eec2b6d28b
TorrieAN <victoria.n.atanasova@gmail.com>
2024-03-26 12:36:18 +0200

    Login user

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,3 +1,20 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 exports.register = (userData) => User.create(userData);
+
+exports.login = async (username, password) => {
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+        throw new Error('Incorrect username or password');
+    }
+
+    const validPass = await bcrypt.compare(password, user.password);
+    if (!validPass) {
+        throw new Error('Incorrect username or password');
+    }
+
+    return user;
+}
