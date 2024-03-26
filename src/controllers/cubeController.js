00bc7e48cebd5c5e2786fd83c57740b0da76f3b7
TorrieAN <victoria.n.atanasova@gmail.com>
2024-03-26 21:00:33 +0200

    Show buttons conditionally

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -35,7 +35,7 @@ router.get('/:cubeId/details', async (req, res) => {
         return res.redirect('/404');
     }
 
-    const isOwner = cube.owner == req.user._id;
+    const isOwner = cube.owner?.toString() == req.user._id;
 
     res.render('cube/details', { cube, isOwner });
 });
