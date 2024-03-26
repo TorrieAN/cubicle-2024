8bea51e94425035fc3ea658ca9bbabb085810891
TorrieAN <victoria.n.atanasova@gmail.com>
2024-03-26 12:11:16 +0200

    Hash password before saving

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: String,
     password: String,
+});
+
+userSchema.pre('save', async function() {
+    const hash = await bcrypt.hash(this.password, 10);
+
+    this.password = hash;
 });
 
 userSchema.virtual('repeatPassword')
