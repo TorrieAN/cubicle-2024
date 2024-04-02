a45c36fab8539307450473c25ca59b73ee4e1e01
TorrieAN <victoria.n.atanasova@gmail.com>
2024-04-02 13:59:02 +0300

    Password and Username verification

--- src/models/User.js
+++ src/models/User.js
@@ -2,8 +2,24 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
+    username: {
+        type: String,
+        required: true,
+        minLength: [5, 'Username must be at least 5 symbols'],
+        match: /^[A-Za-z0-9]+$/,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function(value) {
+                return /^[A-Za-z0-9]+$/.test(value);
+            },
+            message: 'Invalid password!',
+        },
+        minLength: [5, 'Password is too short'],
+    },
 });
 
 userSchema.pre('save', async function() {
