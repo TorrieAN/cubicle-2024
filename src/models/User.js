23ba13b6a48b12846dea45b9613bbf8ef80d37b1
TorrieAN <victoria.n.atanasova@gmail.com>
2024-04-04 17:14:41 +0300

    Register Validation Error Messages Fixes

--- src/models/User.js
+++ src/models/User.js
@@ -4,14 +4,17 @@ const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true,
+        required: [true, 'Username is required'],
         minLength: [5, 'Username must be at least 5 symbols'],
-        match: /^[A-Za-z0-9]+$/,
-        unique: true,
+        match: [/^[A-Za-z0-9]+$/, 'Username must be alphanumeric'],
+        unique: {
+            value: true,
+            message: 'Username already exists',
+        },
     },
     password: {
         type: String,
-        required: true,
+        required: [true, 'Password is required'],
         validate: {
             validator: function(value) {
                 return /^[A-Za-z0-9]+$/.test(value);
@@ -22,18 +25,21 @@ const userSchema = new mongoose.Schema({
     },
 });
 
+
+userSchema.virtual('repeatPassword')
+    .set(function(value) {
+        if (value !== this.password) {
+            throw new Error('Passwords do not match!')
+        }
+    });
+
 userSchema.pre('save', async function() {
     const hash = await bcrypt.hash(this.password, 10);
 
     this.password = hash;
 });
 
-userSchema.virtual('repeatPassword')
-    .set(function(value) {
-        if (value !== this.password) {
-            throw new mongoose.MongooseError('Passwords do not match!')
-        }
-    });
+
 
 const User = mongoose.model('User', userSchema);
 
